c3103ac928b2a488b3204b46255a0798fa933bc6
Jared Hancock <jared@osticket.com>
2016-08-26 11:17:40 -0500

    tasks: Implement enable, disable, and delete for templates

--- include/class.task.php
+++ include/class.task.php
@@ -1746,6 +1746,9 @@ class TaskTemplate extends VerySimpleModel {
             'forms' => array(
                 'reverse' => 'TaskTemplateForm.template',
             ),
+            'instances' => array(
+                'reverse' => 'Task.template'
+            ),
         ),
         'select_related' => array('group'),
     );
